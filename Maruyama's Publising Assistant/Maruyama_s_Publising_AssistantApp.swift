7b14bd4ec227d2ff5c65eed8fd236b5ee6105379
JosNiCa <josnca@gmail.com>
2026-03-23 19:06:46 -0600

    Initial Commit

---
+++ Maruyama's Publising Assistant/Maruyama_s_Publising_AssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  Maruyama_s_Publising_AssistantApp.swift
+//  Maruyama's Publising Assistant
+//
+//  Created by LJD Technology on 23/03/26.
+//
+
+import SwiftUI
+
+@main
+struct Maruyama_s_Publising_AssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
